29d9327095a4e2e81015515c1feea062845b8e02
anthony-chang <anthony20093@gmail.com>
2020-09-05 23:06:36 -0400

    implement free, free_all, and stats

--- mm.c
+++ mm.c
@@ -4,44 +4,26 @@
 
 #include "mm.h"
 
-static header_t* get_free_block(size_t size) {
-    header_t* cur = head;
-    while(cur) {
-        if(cur->st.is_free && cur->st.size >= size) {
-            return cur;
-        }
-        cur = cur->st.next;
-    }
-    return NULL;
-}
-
 void* mm_alloc(size_t size) {
     size_t total_size;
     void* p;
     header_t *header;
 
-    if(size <= 0)
+    if(size <= 0) {
         return NULL;
+    }
 
     pthread_mutex_lock(&lock_g);
 
-    header = get_free_block(size);
-    if(header) { // already existing block
-        header->st.is_free = 0;
-        pthread_mutex_unlock(&lock_g);
-        return (void*) (header + 1);
-    }
-
-    // no block found, we allocate it
     total_size = sizeof(header_t) + size;
     p = malloc(total_size);
     if(!p) {
         pthread_mutex_unlock(&lock_g);
         return NULL;
     }
+    // add to list
     header = p;
     header->st.size = size;
-    header->st.is_free = 1;
     header->st.next = NULL;
     if(!head) {
         head = header;
@@ -50,17 +32,77 @@ void* mm_alloc(size_t size) {
         tail->st.next = header;
     }
     tail = header;
+    // housekeeping
+    stats_g.num_allocced++;
+    stats_g.b_allocced += size;
+
     pthread_mutex_unlock(&lock_g);
     return (void*) (header + 1);
+}
+
+void mm_free(void *p) {
+    header_t *header, **indirect;
+    CHECK_NULLPTR_RET(p);
+
+    pthread_mutex_lock(&lock_g);
+    header = ((header_t*)p) - 1;
+
+    // remove from list
+    indirect = &head;
+    while(*indirect != header) {
+        indirect = &(*indirect)->st.next;
+    }
+    *indirect = header->st.next;
+
+    // housekeeping
+    stats_g.num_allocced--;
+    stats_g.b_allocced -= header->st.size;
+
+    free(header);
+    pthread_mutex_lock(&lock_g);
+}
+
+void mm_free_all() {
+    header_t *cur = head, *next;
+    unsigned int num_freed = 0, num_allocced = stats_g.num_allocced;
+    size_t b_freed = 0, b_allocced = stats_g.b_allocced;
+
+    while(cur) {
+        num_freed++;
+        b_freed += cur->st.size;
+        next = cur->st.next;
+        mm_free((void*) (cur + 1));
+        cur = next;
+    }
+
+    printf("___________________________________________________\n");
+    printf("MEM BLOCKS ALLOCATED: %u | BYTES ALLOCATED: %zu\n", num_allocced, b_allocced);
+    printf("MEM BLOCKS FREED:     %u | BYTES FREED:     %zu\n", num_freed, b_freed);
+    printf("___________________________________________________\n");
 
 }
 
+/* static void print_list() {
+    header_t *cur = head;
+    while(cur) {
+        printf("size=%zu\n", cur->st.size);
+        cur = cur->st.next;
+    }
+} */
 
 int main(int argc, const char** argv) {
     int *p = mm_alloc(sizeof(int));
+    int *p1 = mm_alloc(sizeof(int));
+    int *p2 = mm_alloc(sizeof(int));
+    int *p3 = mm_alloc(sizeof(int));
+    int *p4 = mm_alloc(sizeof(int));
+    int *p5 = mm_alloc(69);
+
     *p = 2934485;
 
     printf("%p, %d\n", p, *p);
+
+    mm_free_all();
 
     return 0;
 }
--- mm.h
+++ mm.h
@@ -3,10 +3,10 @@
 
 #include <pthread.h>
 
-#define CHECK_NULLPTR_RET_NULL(p)                           \
+#define CHECK_NULLPTR_RET(p)                                \
                 do {if(!p) {                                \
                         printf("%s: NULLPTR!\n", __func__); \
-                        return NULL; }} while(0);
+                        return; }} while(0);
 #define CHECK_NULLPTR_RET_VAL(p, val)                       \
                 do {if(!p) {                                \
                         printf("%s: NULLPTR!\n", __func__); \
@@ -17,18 +17,24 @@ typedef struct ALIGN16 { unsigned char s[16]; } ALIGN16;
 typedef union header {
     struct {
         size_t size;
-        unsigned int is_free;
         union header *next;
     } st;
     ALIGN16 align; // align to 16 bytes
 } header_t;
 
+typedef struct mm_stats {
+    unsigned int num_allocced;
+    size_t b_allocced;
+} mm_stats_t;
+
 header_t *head, *tail;
 
 pthread_mutex_t lock_g;
+mm_stats_t stats_g;
 
 void* mm_alloc(size_t size);
 void mm_free(void* p);
+void mm_free_all();
 
 
 
